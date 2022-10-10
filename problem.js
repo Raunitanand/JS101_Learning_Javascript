1d7c297949a4273edc51f00c3673adaad73f6fdd
Raunitanand <48625612+Raunitanand@users.noreply.github.com>
2022-10-10 22:11:45 +0530

    Add files via upload

---
+++ problem.js
@@ -0,0 +1,6 @@
+let name = "Raunit";
+console.log(name);
+name="Pawan Singh";
+console.log(name);
+name="Sangita Singh";
+console.log(name);
---
+++ problem3.js
@@ -0,0 +1,5 @@
+let name="Raunit";
+let age=23;
+console.log(name,age);
+console.log(typeof(name),typeof(age));
+
